fff800d8415a61610521eafc5f098242cd20151c
bsinglet <bsinglet@users.noreply.github.com>
2015-06-30 12:37:58 -0400

    Update text_to_matrix.py
    Added merge_lattice function

--- text_to_matrix.py
+++ text_to_matrix.py
@@ -54,3 +54,44 @@ def swap_all_rows(matrix):
 	for i in range(0, len(matrix)/2):
 		matrix = swap_rows(matrix, i, (len(matrix)-1)-i)
 	return matrix
+
+# split text into five-character "words"
+def wordize(text):
+	out_text = ""
+	for x in range(0, len(text)):
+		if (x % 5 == 0):
+			out_text += " "
+		out_text += text[x]
+	return out_text
+
+# takes two matrices of equal size and merges them into a matrix
+# of twice the dimensions, interleaving values.
+# E.g.,
+#	1 2 3		A B C
+#	4 5 6		D E F
+#	7 8 9		G H I
+#
+#	1 A 2 B 3 C
+#	A 1 B 2 C 3
+#	4 D 5 E 6 F
+#	D 4 E 5 F 6
+#	7 G 8 H 9 I
+#	G 7 H 8 I 9
+def merge_lattice(matrix1, matrix2):
+	out_matrix = []
+	if (len(matrix1) != len(matrix2) or len(matrix1) == 0):
+		return out_matrix
+	size = len(matrix1) * 2
+	for y in range(0, size):
+		my_row = []
+		for x in range(0, size):
+			my_row.append('')
+		out_matrix.append(my_row)
+	
+	for y in range (0, len(matrix1)):
+		for x in range(0, len(matrix1)):
+			out_matrix[(y*2)][x*2] = matrix1[y][x]
+			out_matrix[(y*2)][(x*2)+1] = matrix2[y][x]
+			out_matrix[(y*2)+1][(x*2)+1] = matrix1[y+1][x]
+			out_matrix[(y*2)+1][(x*2)] = matrix2[y+1][x]
+	return out_matrix

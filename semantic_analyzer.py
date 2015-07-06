ada43b3d5e68431380b116d4231d83d087661e74
bsinglet <bsinglet@users.noreply.github.com>
2015-07-06 12:08:37 -0400

    Update semantic_analyzer.py
    Added the evaluate(parseTree) function

--- semantic_analyzer.py
+++ semantic_analyzer.py
@@ -8,6 +8,68 @@ class Token:
 	type = None
 	value = None
 
+my_values = {}
+
+def evaluate(parseTree):
+	global my_value
+	leftChild = parseTree.leftChild
+	rightChild = parseTree.rightChild
+	if parseTree.data.type == 'OPERATOR':
+		# evaluate different operators
+		if parseTree.data.value == '=':
+			# evaluate right child, assign to left child in my_values
+			if leftChild.data.type != 'IDENTIFIER':
+				print "ERROR: Trying to assign to a non-variable: " + leftChild.data.value
+				return -1
+			if rightChild.data.type != 'CONSTANT':
+				rightChild.data.value = evaluate(rightChild)
+			my_values[leftChild.data.value] = rightChild.data.value
+			return leftChild
+		else:
+			# all comparison operators require you to evaluate both the left and the right children first
+			if leftChild.data.type != 'CONSTANT':
+				leftChild.data.value = evaluate(leftChild)
+			if rightChild.data.type != 'CONSTANT':
+				rightChild.data.value = evaluate(rightChild)
+			if parseTree.data.value == '>':
+				# see if the value of the left child is greater than the value of the right child
+				if leftChild.data.value > rightChild.data.value:
+					return True
+				else:
+					return False
+			elif parseTree.data.value == '<':
+				# see if the value of the left child is less than the value of the right child
+				if leftChild.data.value < rightChild.data.value:
+					return True
+				else:
+					return False
+			elif parseTree.data.value == '>=':
+				# see if the value of the left child is greater than or equal to the value of the right child
+				if leftChild.data.value >= rightChild.data.value:
+					return True
+				else:
+					return False
+			elif parseTree.data.value == '<=':
+				# see if the value of the left child is less than or equal to the value of the right child
+				if leftChild.data.value <= rightChild.data.value:
+					return True
+				else:
+					return False
+			elif parseTree.data.value == '!=':
+				# see if the value of the left child is not equal to the value of the right child
+				if leftChild.data.value != rightChild.data.value:
+					return True
+				else:
+					return False
+			elif parseTree.data.value == '==':
+				# see if the value of the left child is equal to the value of the right child
+				if leftChild.data.value == rightChild.data.value:
+					return True
+				else:
+					return False
+	elif parseTree.data.type == 'IDENTIFIER':
+		return my_values[parseTree.data.value]
+
 def example_match_rule(parseTree):
         if parseTree.data.type == 'OPERATOR':
                 if parseTree.leftChild.data.type == 'IDENTIFIER' and parseTree.rightChild.data.type == 'CONSTANT':
